a048402cbd16547f9d41c55a8f9caf1284592539
yuchi20191124 <yuchi20191124@gmail.com>
2026-05-27 17:27:38 +0800

    加速

--- public/sw.js
+++ public/sw.js
@@ -2,33 +2,31 @@ const CACHE_NAME = 'yuchishopping-cache-v2';
 const ASSETS_TO_CACHE = [
   './',
   'index.html',
-  'manifest.json'
+  'manifest.json',
+  'icon-192.png',
+  'icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS_TO_CACHE);
-    }).catch((err) => {
-      console.warn('Pre-caching failed during SW install:', err);
-    })
+    }).then(() => self.skipWaiting())
   );
-  self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((keys) => {
+    caches.keys().then((cacheNames) => {
       return Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
           }
         })
       );
-    })
+    }).then(() => self.clients.claim())
   );
-  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
